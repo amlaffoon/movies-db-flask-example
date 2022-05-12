8fd8eccbc42143e3abf90acef61ba0e8da039e34
Anna Laffoon <ree11194@gmail.com>
2022-05-12 19:12:50 -0400

    added select query

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/movies")
+def movies():
+    connection = sqlite3.connect("movies.db")
+
+    cursor = connection.cursor()
+    
+    rows = cursor.execute("select id, name, year from movies").fetchall()
+    print(rows)
+
+
+
+    return "<p>This is a special psychological picture</p>"
